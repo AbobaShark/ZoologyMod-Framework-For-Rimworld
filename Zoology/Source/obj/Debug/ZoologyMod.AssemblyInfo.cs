21b032ff8c709b68558d0b7c5bec0ea2e9398505
AbobaShark <tirex2222@gmail.com>
2026-05-06 20:40:39 +0700

    Optimisation

--- Zoology/Source/obj/Debug/ZoologyMod.AssemblyInfo.cs
+++ Zoology/Source/obj/Debug/ZoologyMod.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ZoologyMod")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46c50433cac6095e4ecc210055e3160bb7560458")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed346f85858611967be59adee802c305e75e1c83")]
 [assembly: System.Reflection.AssemblyProductAttribute("ZoologyMod")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ZoologyMod")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
